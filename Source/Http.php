675783db5aa60609a8367b025eaeaa38f9003ab3
David de Boer <d.deboer@mttm.nl>
2011-11-14 16:21:25 +0100

    Move file download to its own method

--- Source/Http.php
+++ Source/Http.php
@@ -27,15 +27,26 @@ class Http implements Source
      */
     public function getFile()
     {
-        $temp = tempnam('/tmp', 'data_import');
-        file_put_contents($temp, file_get_contents($this->url));
-
-        $file = new \SplFileObject($temp);
+        $file = $this->downloadFile();
         foreach ($this->filters as $filter) {
             $file = $filter->filter($file);
         }
 
         return $file;
+    }
+
+    /**
+     * Download the file from the internet to a temporary location
+     * 
+     * @return \SplFileObject
+     */
+    public function downloadFile($target = null)
+    {
+        if (!$target) {
+            $target = tempnam('/tmp', 'data_import');
+        }
+        file_put_contents($target, file_get_contents($this->url));
+        return new \SplFileObject($target);
     }
 
     public function addFilter($filter)
