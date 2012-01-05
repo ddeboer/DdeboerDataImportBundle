587c3f02f53a5a4c0bbabd415434c626c14f5609
David de Boer <david@ddeboer.nl>
2012-01-05 20:12:26 +0100

    Add Doctrine reader

---
+++ Reader/DoctrineReader.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Ddeboer\DataImportBundle\Reader;
+
+use Ddeboer\DataImportBundle\Reader;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\Internal\Hydration\IterableResult;
+use Doctrine\ORM\Query;
+
+class DoctrineReader implements Reader
+{
+    protected $objectManager;
+    protected $objectName;
+
+    /**
+     * @var IterableResult
+     */
+    protected $iterableResult;
+
+    public function __construct(ObjectManager $objectManager, $objectName)
+    {
+        $this->objectManager = $objectManager;
+        $this->objectName = $objectName;
+        
+        $query = $this->objectManager->createQuery(
+            sprintf('select o from %s o', $objectName)
+        );
+        $this->iterableResult = $query->iterate(array(), Query::HYDRATE_ARRAY);
+    }
+
+    public function getFields()
+    {
+        return $this->objectManager->getClassMetadata($this->objectName)
+                 ->getFieldNames();
+    }
+
+    public function current()
+    {
+        return current($this->iterableResult->current());
+    }
+
+    public function next()
+    {
+        $this->iterableResult->next();
+    }
+
+    public function key()
+    {
+        return $this->iterableResult->key();
+    }
+
+    public function valid()
+    {
+        return $this->iterableResult->valid();
+    }
+
+    public function rewind()
+    {
+        $this->iterableResult->rewind();
+    }
+}
